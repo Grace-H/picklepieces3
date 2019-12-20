bf97e519ff38a496b308ad5d4c53cac2b8ee2cd3
Grace-H <ghunter3141@gmail.com>
2019-12-20 09:08:19 -0800

    Added Test Button Script

---
+++ Assets/Scripts/ButtonClickedTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClickedTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void PrintClicked(){
+		Debug.Log("Button clicked");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
